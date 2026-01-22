ab515b2dfe0c1a7330d1df29f8f3814764eb18ae
ali abbas <alipasha7017@gmail.com>
2026-01-22 12:30:13 +0530

    new notes

---
+++ loop.js
@@ -0,0 +1,24 @@
+//1: for loop
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//2 while loop
+
+let n = prompt("enter the value of n");
+let i = 0;
+while (i < n) {
+  console.log(i);
+  i++;
+}
+
+//3 do while loop
+
+let n = prompt("enter the value of n");
+let i = 0;
+
+do{
+  console.log(i);
+  i++;
+}
+while(i<n)
