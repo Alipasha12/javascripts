7118b5edbb1f36069f8c344b3981777e5670a165
ali abbas <alipasha7017@gmail.com>
2026-01-23 13:32:14 +0530

    map, filter, reduce

---
+++ mapmethods.js
@@ -0,0 +1,22 @@
+// 1: Map method (it create new array and perform different operation)
+
+let arr = [20, 30, 40, 50, 60]
+
+// simple syntax => arr.map( ()=>{ }) 
+arr.map((value)=>{
+    console.log(value)
+    })
+
+// 2: Filter method  (in this array pass a test and crete new array)
+
+arrfilter = arr.filter((a)=>{
+    return a > 50
+})
+console.log(arrfilter)
+
+// 3: Reduce method (it give a single value)
+
+arrreduce = arr.reduce((h1, h2)=>{
+    return h1 + h2
+})
+console.log(arrreduce)
