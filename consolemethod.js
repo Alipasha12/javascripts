44099bf7949adf8a4adf1eb8359f815c55171048
ali abbas <alipasha7017@gmail.com>
2026-01-24 12:30:05 +0530

    adding console methods

---
+++ consolemethod.js
@@ -0,0 +1,33 @@
+//methods of console
+
+// 1: assert()  
+
+console.assert("") //in this if condition is true they dont give error if false then give assertion fail
+
+
+// 2: clear()0
+
+console.clear() //it is used to clear the console
+
+
+// 3: log()
+
+console.log("hello i am log method") // it give this string value
+
+
+// 4: table()
+
+let obj = {a:1, b:3, c:4, d:7}
+console.table(obj)  //it is used to make table
+
+// 5: warn()
+
+console.warn("hey i am a warning") //its give a warning in console
+
+// 6: error()
+
+console.error("i am error") //it gives error
+
+// 7: info()
+
+console.info("i am information") //its give information without condition and warning
