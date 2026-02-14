722979eef7c3484e3e6ecc009bd9d095d2421747
ali abbas <alipasha7017@gmail.com>
2026-02-14 13:57:30 +0530

    search table

---
+++ tables.js
@@ -0,0 +1,7 @@
+let t = document.body.firstElementChild.firstElementChild //when the element is in body > div > table
+
+console.log(t) //for get table
+
+// for get table rows in html collection
+
+console.log(t.rows)
